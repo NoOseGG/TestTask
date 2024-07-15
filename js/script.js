90dd94ca065e0d1b7c33679810cd46f55a2e698f
NoOse <hoc751@gmail.com>
2024-07-15 03:20:09 +0300

    added notification

--- js/script.js
+++ js/script.js
@@ -13,6 +13,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const inputLogin = document.querySelector('.input-login')
     const inputPassword = document.querySelector('.input-password')
 
+    const notif = document.querySelector('.notification')
+    const notifText = document.querySelector('.notification__text')
 
     demoBtn.addEventListener('click', function () {
         modal.classList.add('modal-active')
@@ -32,12 +34,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
         } else if (!inputPassword.value) {
             notification('Введите пароль')
         } else {
-            modal.remove('modal-active')
+            notification(`Добро пожаловать ${inputLogin.value}`)
+            modal.classList.remove('modal-active')
+            inputLogin.value = ''
+            inputPassword.value = ''
         }
     }
 
     const notification = function (message) {
-        console.log(message);
+        notifText.textContent = message
+        notif.classList.add('notification-active')
+        setTimeout(() => {
+            notif.classList.remove('notification-active')
+        }, 2000)
     }
 
 });
